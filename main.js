672c69d7067feeea6dda710b1c6ce8e08a8cb9c0
ducvr <leotonglinh@gmail.com>
2024-10-20 08:30:39 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+document.addEventListener("DOMContentLoaded", function() {
+  const element = document.querySelector('.happy-woman-day');
+  if (element) {
+    element.textContent = "Happy Women's Day T219";
+  }
+});
